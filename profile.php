c6b42d2c4aee60d6670aa87838a94d0ce48a83ed
Danielle_Nova <novosel.dan@gmail.com>
2024-09-01 07:08:14 +1000

    remake profile.php

--- profile.php
+++ profile.php
@@ -54,9 +54,10 @@
                 <h1 class="hello">Hello, <?php echo htmlspecialchars($_COOKIE['User']); ?></h1>
             </div>
             <div class="col-12">
-                <form method="POST" action="profile.php">
+                <form class="form_align" method="POST" action="profile.php" enctype="multipart/form-data" name="upload">
                     <input type="text" class="form" name="title" placeholder="Title">
                     <textarea name="text" cols="30" rows="10" placeholder="Input text"></textarea>
+                    <input type="file" name="file" /><br>
                     <button type="submit" class="btn_red" name="submit">Save post</button>
                 </form>
             </div>
@@ -84,6 +85,23 @@ if (isset($_POST['submit'])) {
     
     if (!mysqli_query($link, $sql)) die("Failed to add post: " . mysqli_error($link));
 
-    echo "Post added successfully!";
+    echo "Post added successfully!";   
 }
+
+if(!empty($_FILES["file"]))
+    {
+        if (((@$_FILES["file"]["type"] == "image/gif") || (@$_FILES["file"]["type"] == "image/jpeg")
+        || (@$_FILES["file"]["type"] == "image/jpg") || (@$_FILES["file"]["type"] == "image/pjpeg")
+        || (@$_FILES["file"]["type"] == "image/x-png") || (@$_FILES["file"]["type"] == "image/png"))
+        && (@$_FILES["file"]["size"] < 102400))
+        {
+            move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]);
+            echo "Load in:  " . "upload/" . $_FILES["file"]["name"];
+        }
+        else
+        {
+            echo "upload failed!";
+        }
+    }
+
 ?>
